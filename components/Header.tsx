c315cfb59eda78d952be6c46af7120908316ba93
Eduardo188s <eduardodelarosa188@gmail.com>
2025-06-24 18:23:48 -0600

    03 Header listo

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export function Header(){
     return(
-        <header className="w-full  bg-blue-200 text-white py-8 px-6 shadow-md">
+        <header className="w-1/2 bg-[#6381A8] text-white py-8 px-6 shadow-md ml-auto">
             <nav className="max-w-7xl mx-auto flex items-center justify-between">
-                <div className="text-xl font-bold">Sanare</div>
+                <div className="text-xl font-bold"></div>
                 <div className="flex gap-4 text-sm">
-                    <Link href="/" className="hover:underline font-bold rounded-full ">Home</Link>
-                    <Link href="/about" className="hover:underline font-bold">About</Link>
-                    <Link href="/register" className="hover:underline font-bold">Register</Link>
+                    <Link href="/" className="hover font-block rounded-3xl px-5 py-2 text-base font-medium text-white hover:bg-white hover:text-black rounded-full ">Home</Link>
+                    <Link href="/about" className="hover font-block rounded-3xl px-5 py-2 text-base font-medium text-white hover:bg-white hover:text-black rounded-full:underline font-bold">About</Link>
+                    <Link href="/register" className="hover:hover font-block rounded-3xl px-5 py-2 text-base font-medium text-white hover:bg-white hover:text-black rounded-full font-bold">Register</Link>
                 </div>
             </nav>
         </header>
