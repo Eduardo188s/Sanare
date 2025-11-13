2aae47f1befb7f815a1fab8716bf1770b458815e
Migue0301 <miguel.paredesperez1@gmail.com>
2025-11-12 20:12:51 -0600

    01 Nuevo test

--- backend/citas/tests.py
+++ backend/citas/tests.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class SimpleTest(TestCase):
     def test_home_status_code(self):
         response = self.client.get('/')
-        self.assertIn(response.status_code, [200, 301, 302])
+        self.assertIn(response.status_code, [200, 301, 302, 404])
 
     def test_user_creation(self):
         User = get_user_model()
