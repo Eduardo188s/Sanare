8ab184112007f0c26816332c719eb450373ceca3
Migue0301 <miguel.paredesperez1@gmail.com>
2025-11-12 20:09:23 -0600

    Nuevo test

--- backend/citas/tests.py
+++ backend/citas/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_home_status_code(self):
+        response = self.client.get('/')
+        self.assertIn(response.status_code, [200, 301, 302])
+
+    def test_user_creation(self):
+        User = get_user_model()
+        user = User.objects.create(username="testuser")
+        self.assertEqual(user.username, "testuser")
